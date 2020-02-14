1186c32d2ca984ab33e522990dddef32b164ec73
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2020-02-13 23:44:33 -0500

    pretty sure insert implementation is correct

--- src/mansisak_assignment2/BST.java
+++ src/mansisak_assignment2/BST.java
@@ -19,7 +19,31 @@ public class BST implements BST_Interface {
 	
 	@Override
 	public boolean insert(String s) {
-		// TODO Auto-generated method stub
+		if(this.contains(s)) {
+			return false;
+		}
+		BST_Node new_node = new BST_Node(s);
+		if(size == 0) {
+			root = new_node;
+		}
+		BST_Node current_node = root;
+		BST_Node parent_node = null;
+		while(current_node != null) {
+			parent_node = current_node;
+			if(s.compareTo(current_node.data)<0) {
+				current_node = current_node.left;
+			}
+			else {
+				current_node = current_node.right;
+			}
+		}
+		if(s.compareTo(parent_node.data)<0) {
+			parent_node.left = new BST_Node(s);
+		}
+		else {
+			parent_node.right = new BST_Node(s);
+		}
+		size++;
 		return true;
 	}
 
@@ -28,7 +52,11 @@ public class BST implements BST_Interface {
 		if (size == 0) {
 			return false;
 		}
-		// TODO Auto-generated method stub
+		if (!this.contains(s)) {
+			return false;
+		}
+		//TODO SOMEHOW ID THE CORRECT CELL AND REMOVE IT AND REARRANGE THE TREE
+		size--;
 		return false;
 	}
 
@@ -37,8 +65,11 @@ public class BST implements BST_Interface {
 		if (size == 0) {
 			return null;
 		}
-		// TODO Auto-generated method stub
-		return null;
+		BST_Node current = root;
+		while(current.left != null) {
+			current = current.left;
+		}
+		return current.data;
 	}
 
 	@Override
@@ -46,8 +77,9 @@ public class BST implements BST_Interface {
 		if (size == 0) {
 			return null;
 		}
-		// TODO Auto-generated method stub
-		return null;
+		BST_Node current = root;
+		//TODO do this meathods por favor
+		return current.data;
 	}
 
 	@Override
@@ -77,6 +109,7 @@ public class BST implements BST_Interface {
 		if (size == 0) {
 			return -1;
 		}
+		//TODO finish this
 		return 0;
 	}
 

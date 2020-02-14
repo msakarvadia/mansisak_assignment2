4317bd71edb1dbd144e221889cfb40dbcfb64305
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2020-02-13 22:12:32 -0500

    all additional files needed for this project

---
+++ src/mansisak_assignment2/RunTests.java
@@ -0,0 +1,8 @@
+package mansisak_assignment2;
+import gradingTools.comp410s20.assignment2.testcases.Assignment2Suite;
+
+public class RunTests {
+  public static void main(String[] args){ //runs Assignment 2 oracle tests
+    Assignment2Suite.main(args);
+  }
+}
